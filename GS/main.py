fd69b31958d03cb10f01580928a89be67733f784
Дмитрий Минор <dmitrijminor@MacBook-Air-Dmitrij.local>
2019-11-10 16:41:39 +0700

    New

--- GS/main.py
+++ GS/main.py
@@ -4,7 +4,7 @@ from modules.add_object_to_gsheets import *
 from modules.parser_gs import *
 MAIN_SPREADSHEET_ID = '1cqJVtaHWTozVZFdKBBUNwCqET63aY9JZVgXRuqwT_aA'
 pathJsonScript = '/Users/dmitrijminor/tests/cdr'
-SAMPLE_RANGE_NAME = 'SIP!1:4'
+SAMPLE_RANGE_NAME = 'Лист3!1:16'
 
 data_sheet = show_sheets(main_fun(),MAIN_SPREADSHEET_ID,SAMPLE_RANGE_NAME)
 listpath = (loadjson(pathJsonScript))
@@ -12,10 +12,13 @@ listpath = (loadjson(pathJsonScript))
 #print(parse_json(listpath)[2])
 #print(search_tag())
 #print(data_sheet[0])
-key_columns = Parser().pars_key_columns(data_sheet)
-print(key_columns)
+pars = Parser()
+key_colunns_dict = pars.pars_key_columns(data_sheet)
+print(key_colunns_dict)
 print('-'*100)
 
 for str_data in data_sheet[1:]:
-    pars = Parser().new_pars(str_data, key_columns)
-    print(pars)
+    print(pars.parsing_in_parts(str_data, key_colunns_dict))
+
+print('-'*100)
+print(pars.main_parts.table_assembly())
--- GS/modules/parser_gs.py
+++ GS/modules/parser_gs.py
@@ -2,18 +2,54 @@ import itertools
 
 class Main_part():
     '''Класс раздела'''
-    def __init__(self, root_part, name, desc, tag):
+    def __init__(self,name,desc,tag):
         '''Инициализация атрибутов:
         name - название раздела
-        desc - описание раздела
+        desc - описание раздела¡
         tag - тег раздела
         '''
-        self.root_part = 'ROOT'
+        self.name = name
         self.desc = desc
         self.tag = tag
 
+        self.root_part = {'Root':{}}
+        self.parts = {}
+        self.section = {}
+        self.test_keys = {}
+
+    def add_part(self,part,value):
+        self.parts[part] = value
+
+    def add_section(self,section,value):
+        self.section[section] = value
+
+    def add_test_keys(self,test_keys,value):
+        self.test_keys[test_keys] = value
+
+    def table_assembly(self):
+        for part, item in self.parts.items():
+            tag_part = item.get('TAG').split(sep=' ')
+            tag_part = [tag.replace(',','') for tag in tag_part]
+            item['SUBSECTION'] = {}
+            self.root_part['Root'][part] = item
+            for key, value in self.section.items():
+                tag_section = value.get('TAG').split(sep=' ')
+                tag_section = [tag.replace(',', '') for tag in tag_section]
+                if tag_part[0] == tag_section[0].replace(',',''):
+                    value['TEST KEYS'] = {}
+                    self.root_part['Root'][part]['SUBSECTION'][key] = value
+                    for k, v in self.test_keys.items():
+                        tag_test_keys = v.get('TAG').split(sep=' ')
+                        tag_test_keys = [tag.replace(',', '') for tag in tag_test_keys]
+                        if tag_section[0] == tag_test_keys[0] and tag_section[1] == tag_test_keys[1]:
+                            self.root_part['Root'][part]['SUBSECTION'][key]['TEST KEYS'][k] = v
+                            print('-' * 100)
+        return self.root_part
+
 class Parser():
     '''Класс определяет как тип строки пришел с таблиц (Раздел, подраздел или тест-кейс)'''
+    def __init__(self):
+        self.main_parts = Main_part('Root', 'Главный раздел', 'root')
 
     def pars_key_columns(self, data_gs):
         result = [value for value in data_gs[0]]
@@ -22,50 +58,20 @@ class Parser():
             key_colunns_dict[str(item).upper()] = item
         return key_colunns_dict
 
-    def new_pars(self, data_gs, key_columns):
+    def parsing_in_parts(self, data_gs, key_colunns_dict):
         #Парсим раздел
         result = [value for value in data_gs]
-        str_dict = dict(itertools.zip_longest(key_columns, result, fillvalue=''))
+        str_dict = dict(itertools.zip_longest(key_colunns_dict, result, fillvalue=''))
         if str_dict['TAG'] != '':
-            tag_str = str(str_dict.get('TAG', '')).split(sep=',')
+            tag_str = str(str_dict.get('TAG', '')).split(sep=' ')
             if len(tag_str) == 1:
-                print('This is part')
-        return str_dict
+                print('This is Part')
+                self.main_parts.add_part(str_dict.get('SECTION'),str_dict)
+            elif len(tag_str) > 1 and str_dict['TEST CASE'] == '':
+                print('This in SECTION')
+                self.main_parts.add_section(str_dict.get('SECTION'),str_dict)
+            elif len(tag_str) > 1 and str_dict['TEST CASE'] != '':
+                print('This is Test keys')
+                self.main_parts.add_test_keys(str_dict.get('TEST CASE'),str_dict)
+        return
 
-
-    def pars_gs(self, *data_gs):
-        result = [value for a in data_gs for value in a]
-        m = 0
-        j = 0
-        res_dict = dict()
-        #Парсим раздел
-        status_pars = False
-        for id, item in enumerate(result):
-            res_dict[int(id)] = item
-        for i in range(len(res_dict)):
-            if i >= 2 and i <= 7:
-                if res_dict[i] == '':
-                    m += 1
-                    if m == 6:
-                        print('This is main part')
-                        status_pars = True
-        #Парсим подраздел
-        if res_dict[0] == '':
-            if res_dict[1] != '':
-                if res_dict[2] == '':
-                    if res_dict[3] != '':
-                        if res_dict[4] != '':
-                            print('This is part')
-                            status_pars = True
-        #Парсим тест-кейс
-        for i in range(len(res_dict)):
-            if i >= 1 and i <= 4:
-                if res_dict[i] != '':
-                    j += 1
-                    if j == 4:
-                        print('This is test keys')
-                        status_pars = True
-        #Если ничего не нашли
-        if status_pars == False:
-            print('Огнев заебал, пиши нормально тесты')
-        return result
